1117aebdec4f496a4d7f90554c0706a1a9d970e1
Kai Luo <gluokai@gmail.com>
2022-08-12 21:48:23 +0800

    Default to Roboto Mono

--- .wezterm.lua
+++ .wezterm.lua
@@ -4,7 +4,7 @@ return {
   default_prog = {'/bin/bash'},
   initial_cols = 120,
   initial_rows = 32,
-  font = wezterm.font('JetBrains Mono'),
+  font = wezterm.font('Roboto Mono'),
   font_size = 12,
   color_scheme = 'Vaughn',
   exit_behavior = 'Close',
