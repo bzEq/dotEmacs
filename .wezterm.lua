f408f2b11cf4babb16279ccba03a142f67d98e55
Kai Luo <gluokai@gmail.com>
2024-04-13 10:38:51 +0800

    Disable ligatures

--- .wezterm.lua
+++ .wezterm.lua
@@ -6,6 +6,7 @@ return {
   initial_rows = 32,
   font = wezterm.font('Roboto Mono'),
   font_size = 12,
+  harfbuzz_features = {"calt=0", "clig=0", "liga=0"},
   color_scheme = 'Vaughn',
   exit_behavior = 'Close',
 }
